4218ef5a6073dcb54aabae3617dbeda9e2db4f13
Jan Vansteenkiste <jan@vstone.eu>
2013-01-31 02:18:49 +0100

    Simplified the is_example fact example a bit

--- lib/facter/is_example.rb
+++ lib/facter/is_example.rb
@@ -1,5 +1,10 @@
 Facter.add(:is_example) do
   setcode do
-    is_example = (Facter.value('fqdn') =~ /^(.*\.)?example\.com$/) == nil ? false : true
+    if (Facter.value('fqdn') =~ /^(.*\.)?example\.com$/) == nil
+      is_example = false
+    else
+      is_example = true
+    end
+    is_example
   end
 end
